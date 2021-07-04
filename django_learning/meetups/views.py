98b8589fb0db5bd78f6874de352a797260c629bf
Abuubkar <abubakark1998@gmail.com>
2021-07-04 17:09:43 +0500

    added db connection with admin and fetch data from db to views.py

--- django_learning/meetups/views.py
+++ django_learning/meetups/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Meetup
+
 # Create your views here.
 
 def index(request):
     #no need to specify templates folder in PATH
-    meetups = [
-        {
-            'title': 'A First Meetup', 
-            'location': 'Lahore', 
-            'slug': 'a-first-meetup' 
-            },
-        {
-            'title': 'A Second Meetup', 
-            'location': 'Karachi', 
-            'slug': 'b-first-meetup' 
-            }
-    ]
+    meetups = Meetup.objects.all()
+    
     return render(request,'meetups/index.html',{
         'show_meetups':True,
         'meetups':meetups
